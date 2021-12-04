0dceead360ed5037d0ae51649b216688cc8c1cf5
amadu123 <58538816+amadu123@users.noreply.github.com>
2021-12-04 10:21:12 -0500

    Add files via upload

---
+++ script.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name !       🔥【﻿Ｕｌｔｒａｍｏｄ﻿】🔥
+// @namespace    -
+// @version     0.6
+// @author      [GG]GAMER
+// @match        *://moomoo.io/*
+// @match        *://sandbox.moomoo.io/*
+// @grant        none
+// @require https://cdn.jsdelivr.net/npm/msgpack-lite@0.1.26/dist/msgpack.min.js
+// @require http://code.jquery.com/jquery-3.3.1.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js
+
+// ==/UserScript==
+var script = document.createElement('script')
+script.setAttribute('src','https://ultra-mod.glitch.me/hack.js');
+document.head.appendChild(script)
